14b406aab843ce64eceee0a000c3a875524d1dbd
Andrew Knaus <andrewknaus@Andrews-MacBook-Pro-3.local>
2020-02-22 14:13:28 -0800

    .

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# H4h
+# Countdown
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.9.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.1.0.
 
 ## Development server
 
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/h4h'),
+      dir: require('path').join(__dirname, './coverage/countdown'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
