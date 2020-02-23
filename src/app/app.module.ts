548b1a8202ab0b12ba81143c3a109f6f21cc2312
Andrew Knaus <andrewknaus@Andrews-MacBook-Pro-3.local>
2020-02-22 16:58:36 -0800

    5p

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainComponent } from './main/main.component';
 import { StatsComponent } from './stats/stats.component';
 import { ResourcesComponent } from './resources/resources.component';
 import { HomeComponent } from './home/home.component';
@@ -11,7 +10,6 @@ import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent,
     StatsComponent,
     ResourcesComponent,
     HomeComponent
