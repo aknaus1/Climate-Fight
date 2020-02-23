720c23f237109e3c7ba4fee8ea631cfabcd06be9
Andrew Knaus <andrewknaus@Andrews-MacBook-Pro-3.local>
2020-02-22 16:51:55 -0800

    4:51p

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Climate Tracker';
-  scroll(el: HTMLElement) {
-    el.scrollIntoView();
-  }
+  title = 'World Climate Tracker';
 }
