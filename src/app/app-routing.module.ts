d21b7065ee47b46de790b12086b0ba7290ebdb84
Andrew Knaus <andrewknaus@Andrews-MacBook-Pro-3.local>
2020-02-22 16:17:47 -0800

    broken up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { StatsComponent } from './stats/stats.component';
+import { ResourcesComponent } from './resources/resources.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent},
+  { path: 'stat', component: StatsComponent },
+  { path: 'rsc', component: ResourcesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Climate Tracker';
+  scroll(el: HTMLElement) {
+    el.scrollIntoView();
+  }
 }
